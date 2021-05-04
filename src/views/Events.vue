0f8552acd897e7637fb7435b872670a94a069205
Jakob van Santen <jvansanten@gmail.com>
2021-05-04 14:39:01 +0200

    Filter events by process name

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,7 +1,39 @@
 <template>
-  <b-container>
+  <b-container fluid>
+    <b-row>
+      <b-col lg="6" class="my-1">
+        <b-form-group
+          label="Filter"
+          label-for="filter-input"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              id="filter-input"
+              v-model="filter"
+              type="search"
+              placeholder="Type to Search"
+            ></b-form-input>
+
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''"
+                >Clear</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </b-row>
     <div id="events-table">
-      <b-table :busy="isLoading" :items="events" :fields="fields">
+      <b-table
+        :busy="isLoading"
+        :items="events"
+        :fields="fields"
+        :filter="filter"
+      >
         <!-- A virtual column -->
         <template #cell(successmark)="data">
           <div v-if="data.item.success">
@@ -29,6 +61,8 @@ export default {
   data() {
     return {
       isLoading: true,
+      filter: null,
+      filterOn: [],
       fields: [
         { key: "run" },
         { key: "process" },
