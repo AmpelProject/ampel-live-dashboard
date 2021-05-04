afc11e1863554201f15b9796fbdc51a7ee896881
Jakob van Santen <jvansanten@gmail.com>
2021-05-04 18:44:18 +0200

    Add production build

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    BACKEND_AUTH_URL: "http://localhost:8000/api",
+    BACKEND_AUTH_URL: "https://ampel.zeuthen.desy.de/api",
     BACKEND_URL: "https://ampel.zeuthen.desy.de/api",
     token: undefined,
     token_payload: undefined,
