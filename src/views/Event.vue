d5a76182a524ef50578997840e9625e59b3471d4
Jakob van Santen <jvansanten@gmail.com>
2021-05-06 14:36:00 +0200

    Paginate logs

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,50 +1,145 @@
 <template>
   <b-container>
-    <h1>Run {{ $route.params.id }}</h1>
+    <b-row>
+      <b-col>
+        <h3>Run {{ $route.params.id }}</h3>
+      </b-col>
+      <b-col>
+        <b-form-group
+          label="Filter"
+          label-for="filter-input"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              id="filter-input"
+              v-model="filter"
+              type="search"
+              placeholder="Type to Search"
+            ></b-form-input>
 
-    <div id="events-table">
-      <b-table :busy="isLoading" :items="logs" :fields="fields"> </b-table>
-    </div>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''"
+                >Clear</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group
+          label="Items"
+          label-for="per-page-input"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-select
+              id="per-page-input"
+              v-model="perPage"
+              :options="perPageOptions"
+            ></b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-pagination
+          id="page-input"
+          align="right"
+          v-model="currentPage"
+          :totalRows="totalRows"
+          :per-page="perPage"
+        ></b-pagination>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-table
+        id="events-table"
+        borderless
+        small
+        :items="loadData"
+        :filter="filter"
+        :fields="fields"
+        :current-page="currentPage"
+        :per-page="perPage"
+      >
+      </b-table>
+    </b-row>
   </b-container>
 </template>
 
-<script>
+<script lang="ts">
+import { BvTableCtxObject } from "bootstrap-vue";
 import axios from "axios";
 
 export default {
   name: "Event",
   data() {
     return {
-      isLoading: false,
-      logs: [],
       fields: [
         "timestamp",
-        "tier",
         "location",
         "level",
         { key: "message", tdClass: "text-left" },
       ],
+      filter: undefined,
+      currentPage: 1,
+      perPage: 20,
+      totalRows: 0,
+      perPageOptions: [10, 20, 50, 100, 1000],
     };
   },
   created() {
-    this.loadData();
+    this.countRows();
+  },
+  watch: {
+    filter: function() { this.countRows(); }
   },
   methods: {
-    loadData() {
-      this.isLoading = true;
+    loadData(ctx: BvTableCtxObject, callback: Function): void {
+      console.log(ctx);
       axios
         .get(
           this.$store.state.BACKEND_URL + "/live/logs/" + this.$route.params.id,
           {
             headers: { Authorization: "bearer ${this.$store.state.token}" },
+            params: {
+              skip: (ctx.currentPage - 1) * ctx.perPage,
+              limit: ctx.perPage,
+              filter: ctx.filter,
+            },
           }
         )
         .then((response) => {
-          this.logs = response.data.logs;
-          this.isLoading = false;
+          callback(response.data.logs);
         })
         .catch((error) => {
-          this.isLoading = false;
+          console.log(error);
+          callback([]);
+        });
+    },
+    countRows(): void {
+      console.log("recount");
+      axios
+        .get(
+          this.$store.state.BACKEND_URL +
+            "/live/logs/" +
+            this.$route.params.id +
+            "/length",
+          {
+            headers: { Authorization: "bearer ${this.$store.state.token}" },
+            params: { filter: this.filter },
+          }
+        )
+        .then((response) => {
+          this.totalRows = response.data.length;
+        })
+        .catch((error) => {
           console.log(error);
         });
     },
