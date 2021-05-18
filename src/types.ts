7e9c6e171655d9e1bdb7cad7827f8e3a9a36f278
Jakob van Santen <jvansanten@gmail.com>
2021-05-18 10:46:04 +0200

    Prettify exception listing

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
-
 export interface EventSummary {
-    count: number,
-    successes: number,
-    failures: number,
-    process: string,
-    tier: number,
-};
+  count: number;
+  successes: number;
+  failures: number;
+  process: string;
+  tier: number;
+}
+
+export interface TroubleDoc {
+  exception: Array<string>;
+  _exc_content?: { error: string; message: string };
+}
--- src/views/Troubles.vue
+++ src/views/Troubles.vue
@@ -1,25 +1,31 @@
 <template>
-  <b-container>
-    <b-row v-for="doc in troubles" :key="doc.timestamp">
-      <div>
-        <b-card-header>
-          <router-link :to="'/event/' + doc.run">{{ doc.process }}</router-link>
-        </b-card-header>
-        <b-card class="text-monospace">
-          <small v-for="line in doc.exception" :key="line">
-            {{ line }}
-            <br />
-          </small>
-        </b-card>
-      </div>
-    </b-row>
-  </b-container>
+  <div>
+    <b-list-group>
+      <b-list-group-item
+        href="#"
+        class="flex-column align-items-start"
+        v-for="doc in troubles"
+        :key="doc.timestamp"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{ doc._exc_content.error }}</h5>
+          <small>{{ formatDate(doc.timestamp) }}</small>
+        </div>
+
+        <p class="mb-1">{{ doc._exc_content.message }}</p>
+
+        <small>{{ doc.process }}</small>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
 
 <script lang="ts">
 import { AxiosResponse } from "axios";
 import Vue, { VueConstructor } from "vue";
 import EventQuery from "@/mixins/EventQuery";
+import { formatDate } from "@/lib/utils";
+import { TroubleDoc } from "@/types";
 
 // Teach TypeScript about mixins
 // see: https://forum.vuejs.org/t/using-mixins-with-typescript/24256/18
@@ -30,13 +36,36 @@ export default (Vue as VueConstructor<
   mixins: [EventQuery],
   data() {
     return {
-      troubles: [],
+      troubles: [] as Array<TroubleDoc>,
     };
   },
+  watch: {
+    timeRangeQuery() {
+      this.loadData();
+    },
+  },
   created() {
-    this.fetch("/live/troubles", (response: AxiosResponse) => {
-      this.troubles = response.data.troubles;
-    });
+    this.loadData();
+  },
+  methods: {
+    loadData() {
+      this.fetch("/live/troubles", (response: AxiosResponse) => {
+        this.troubles = response.data.troubles;
+        for (var doc of this.troubles) {
+          doc._exc_content = this.getExceptionLine(doc.exception);
+        }
+      });
+    },
+    getExceptionLine(lines: Array<string>): { error: string; message: string } {
+      for (var line of lines.slice().reverse()) {
+        const match = line.match(/^(.*Error):\s+(.*)/);
+        if (match) {
+          return { error: match[1], message: match[2] };
+        }
+      }
+      return { error: "Exception", message: "" };
+    },
+    formatDate: formatDate,
   },
 });
 </script>
