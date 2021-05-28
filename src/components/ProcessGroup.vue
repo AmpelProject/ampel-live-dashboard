5fb318931016ed4403ce34123c92089226d807bc
Jakob van Santen <jvansanten@gmail.com>
2021-05-28 14:23:42 +0200

    ProcessGroup: prettify

--- src/components/ProcessGroup.vue
+++ src/components/ProcessGroup.vue
@@ -1,27 +1,29 @@
 <template>
-  <div @click="expand = !expand">
-    <b-card-header>
-      <b-icon :icon="expand ? 'chevron-down' : 'chevron-right'"></b-icon>
-      <b-badge variant="light">T{{ summary.tier }}</b-badge>
-      {{ summary.process.replaceAll("|", " ") }}
-      <b-badge
-        pill
-        variant="danger"
-        v-if="summary.failures"
-        :to="'/events/' + summary.process + '/troubles'"
-        >{{ summary.failures }}</b-badge
-      >
-      <b-badge pill variant="light">{{ summary.count }}</b-badge>
-    </b-card-header>
-    <b-card no-body>
-      <b-card-body v-if="expand">
-        <event-details
-          :name="summary.process"
-          :timeRangeQuery="timeRangeQuery"
-        />
-      </b-card-body>
-    </b-card>
-  </div>
+  <b-container @click="expand = !expand" class="mt-1">
+    <b-card-group deck>
+      <b-card no-body>
+        <b-card-header header-tag="h6">
+          <b-icon :icon="expand ? 'chevron-down' : 'chevron-right'"></b-icon>
+          <b-badge variant="light">T{{ summary.tier }}</b-badge>
+          {{ summary.process.replaceAll("|", " ") }}
+          <b-badge
+            pill
+            variant="danger"
+            v-if="summary.failures"
+            :to="'/events/' + summary.process + '/troubles'"
+            >{{ summary.failures }}</b-badge
+          >
+          <b-badge pill variant="light">{{ summary.count }}</b-badge>
+        </b-card-header>
+        <b-card-body v-if="expand">
+          <event-details
+            :name="summary.process"
+            :timeRangeQuery="timeRangeQuery"
+          />
+        </b-card-body>
+      </b-card>
+    </b-card-group>
+  </b-container>
 </template>
 
 <script lang="ts">
