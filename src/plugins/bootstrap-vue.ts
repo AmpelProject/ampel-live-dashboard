9324a785e35ac2640ea35b898e65bc02e43320d4
Jakob van Santen <jvansanten@gmail.com>
2021-05-28 15:04:07 +0200

    Import subset of bootstrap-vue
    Reduces entrypoint size from 1.4 MB to ~620 kB

--- src/plugins/bootstrap-vue.ts
+++ src/plugins/bootstrap-vue.ts
@@ -1,8 +1,56 @@
 import Vue from "vue";
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import {
+  TablePlugin,
+  PaginationPlugin,
+  NavPlugin,
+  NavbarPlugin,
+  DropdownPlugin,
+  CardPlugin,
+  ListGroupPlugin,
+  SidebarPlugin,
+  BadgePlugin,
+  BIcon,
+  BIconClockHistory,
+  BIconCheck,
+  BIconCheckCircleFill,
+  BIconChevronRight,
+  BIconChevronDown,
+  BIconDownload,
+  BIconFilter,
+  BIconGithub,
+  BIconArrowRepeat,
+  BIconX,
+  BIconClipboard,
+  BIconXSquareFill,
+  BIconQuestionCircle,
+} from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
+// components
+Vue.use(TablePlugin);
+Vue.use(PaginationPlugin);
+Vue.use(NavPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(DropdownPlugin);
+Vue.use(CardPlugin);
+Vue.use(ListGroupPlugin);
+Vue.use(SidebarPlugin);
+Vue.use(BadgePlugin);
+
+// icons
+Vue.component("BIcon", BIcon);
+Vue.component("BIconClockHistory", BIconClockHistory);
+Vue.component("BIconCheck", BIconCheck);
+Vue.component("BIconCheckCircleFill", BIconCheckCircleFill);
+Vue.component("BIconChevronRight", BIconChevronRight);
+Vue.component("BIconChevronDown", BIconChevronDown);
+Vue.component("BIconDownload", BIconDownload);
+Vue.component("BIconFilter", BIconFilter);
+Vue.component("BIconGithub", BIconGithub);
+Vue.component("BIconArrowRepeat", BIconArrowRepeat);
+Vue.component("BIconX", BIconX);
+Vue.component("BIconClipboard", BIconClipboard);
+Vue.component("BIconXSquareFill", BIconXSquareFill);
+Vue.component("BIconQuestionCircle", BIconQuestionCircle);
